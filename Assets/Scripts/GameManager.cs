71c286fa11e1575c22c6b1f4250c6dcddeffce49
Newton Duarte Cavalcanti <newton_@hotmail.com>
2023-10-20 06:19:18 -0300

    feat: add ui and audio

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,20 +1,23 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
     [SerializeField] float loadDelay = 0.75f;
+    [SerializeField] TextMeshProUGUI coinsText;
+    [SerializeField] TextMeshProUGUI livesText;
+    [SerializeField] AudioClip bonusLivesClip;
 
     Player player;
     int playerLives = 3;
+    int coins = 0;
 
     Coroutine gameOverRoutine;
     Coroutine resetLevelRoutine;
-
-    GameManager instance;
 
     void Awake()
     {
@@ -22,16 +25,40 @@ public class GameManager : MonoBehaviour
         player = FindAnyObjectByType<Player>();
     }
 
+    void Start()
+    {
+        UpdateUI();
+    }
+
+    private void UpdateUI()
+    {
+        livesText.text = $"x{playerLives}";
+        coinsText.text = $"x{coins}";
+    }
+
+    public void AddCoins(int value)
+    {
+        coins += value;
+
+        if (coins >= 100)
+        {
+            playerLives++;
+            coins = 0;
+            PlaySFX(bonusLivesClip);
+        }
+
+        UpdateUI();
+    }
+
     void ManageSingleton()
     {
-        if (instance != null)
+        int numOfGameManager = FindObjectsByType<GameManager>(FindObjectsSortMode.None).Length;
+        if (numOfGameManager > 1)
         {
-            gameObject.SetActive(false);
             Destroy(gameObject);
         }
         else
         {
-            instance = this;
             DontDestroyOnLoad(gameObject);
         }
     }
@@ -79,5 +106,10 @@ public class GameManager : MonoBehaviour
     {
         yield return new WaitForSeconds(loadDelay);
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    void PlaySFX(AudioClip clip)
+    {
+        AudioManager.GetInstance().PlaySFX(clip);
     }
 }
